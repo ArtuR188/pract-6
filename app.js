9f2b00d576a940efdd4de5714b2718039aaaaad0
ArtuR188 <144206907+ArtuR188@users.noreply.github.com>
2024-01-13 20:37:00 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+import './app.js';
+import QuestionsComponent from './questions';
+
+function App () {
+  return (
+    <div className="App">
+      <QuestionsComponent />
+    </div>
+  );
+}
+
+export default App;
